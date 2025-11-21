514f87f21f9b15f0a211325a8e31143ae06477c3
Donghun <2dh2@naver.com>
2025-11-21 17:16:25 +0900

    project init

---
+++ src/test/java/com/example/konect/KonectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.konect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
